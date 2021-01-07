c518ae5c8049a08f3a51e1f0678f0549c7cfbfd6
Majorann Kode <majorann.thevarajah@nmbu.no>
2021-01-07 12:59:29 +0100

    Create animals.py and animal class

--- biosim/animals.py
+++ biosim/animals.py
@@ -2,3 +2,10 @@
 
 __author__ = "Majorann Thevarjah & Anish Thangalingam"
 __email__ = "Majorann.thevarajah@nmbu.no & Anish.thangalingam@nmbu.no"
+
+import math
+import numpy as np
+import random
+
+class Animal:
+    
