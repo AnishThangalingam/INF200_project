a98ebe90ca9eb671597940a80d87a06c5ef630f5
Majorann Kode <majorann.thevarajah@nmbu.no>
2021-01-07 13:12:36 +0100

    Create init function for animal class

--- biosim/animals.py
+++ biosim/animals.py
@@ -8,4 +8,23 @@ import numpy as np
 import random
 
 class Animal:
-    
+    parameters = {}
+
+    def __init__(self, age=None, weight = None):
+        """
+        # legg inn kommentar
+        """
+        if age is None:
+            self.age = 0
+        elif age < 0:
+            raise ValueError('The age must be non-negative')
+        else:
+            self.age = age
+
+        if weight is None:
+            self.weight = self.calculated_weight()
+        elif weight < 0:
+            raise ValueError('The weight must be non-negative')
+        else:
+            self.weight = weight
+        self.fitness = self.get_fitness
