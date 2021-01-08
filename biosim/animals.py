442d68976d544f63262978a1d48d28bd1610df9a
Majorann Kode <majorann.thevarajah@nmbu.no>
2021-01-08 14:17:30 +0100

    Start to create Carnivore class

--- biosim/animals.py
+++ biosim/animals.py
@@ -179,3 +179,28 @@ class Herbivore(Animal):
 
     def __init__(self, age=None, weight=None):
         super().__init__(age, weight)
+
+
+class Carnivore(Animal):
+    """Carnivore class is a subclass to class Aninal. Legge til mere tekst"""
+
+    parameters = {
+        "w_birth": 6.0,
+        "sigma_birth": 1.0,
+        "beta": 0.75,
+        "eta": 0.125,
+        "a_half": 40.0,
+        "phi_age": 0.3,
+        "w_half": 4.0,
+        "phi_weight": 0.4,
+        "mu": 0.4,
+        "gamma": 0.8,
+        "zeta": 3.5,
+        "xi": 1.1,
+        "omega": 0.8,
+        "F": 50.0,
+        "DeltaPhiMax": 10.0,
+    }
+
+    def __init__(self, age=None, weight=None):
+        super().__init__(age, weight)
