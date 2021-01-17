124db8580d9b8d1b8b1ea5a1a28692426602c36a
Majorann Kode <majorann.thevarajah@nmbu.no>
2021-01-17 11:48:04 +0100

    Test whether parameter_set function raise error

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -4,6 +4,7 @@ __author__ = "Majorann Thevarjah & Anish Thangalingam"
 __email__ = "Majorann.thevarajah@nmbu.no & Anish.thangalingam@nmbu.no"
 
 from biosim.animals import Herbivore, Carnivore
+import pytest
 
 
 def test_parameter_set():
@@ -27,3 +28,23 @@ def test_weight_at_birth():
 
     carnivore = Carnivore(0, None)
     assert carnivore.weight > 0
+
+
+def test_raise_error_parameter_set():
+    """
+    Tests whether parameter_set function raise Key Error and value error when
+    the new parameter is a invalid parameter and when it has negative value
+
+    Set half instead of a_half and and chek if it raise key error
+
+    Set -32 for F instead of non-negative value and check if it raise Value error
+    """
+    new_parameters_to_set = {"half": 100, "F": 2}
+
+    with pytest.raises(KeyError):
+        Carnivore.parameter_set(new_parameters_to_set)
+
+    new_parameter_with_negative_value = {"F": -32}
+
+    with pytest.raises(ValueError):
+        Carnivore.parameter_set(new_parameter_with_negative_value)
