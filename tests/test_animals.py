674ad8759a5002bba4600b7c8c69c0a95371cdc3
Majorann Kode <majorann.thevarajah@nmbu.no>
2021-01-17 11:33:39 +0100

    Testing parameter_set function from Animal class

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -3,3 +3,15 @@
 __author__ = "Majorann Thevarjah & Anish Thangalingam"
 __email__ = "Majorann.thevarajah@nmbu.no & Anish.thangalingam@nmbu.no"
 
+from biosim.animals import Herbivore, Carnivore
+
+
+def test_parameter_set():
+    """
+    Testing the parameter_set function are able to update parameters
+    """
+
+    new_parameters = {"a_half": 100, "F": 2}
+    present_parameters = Herbivore.parameters
+    new_parameters = Herbivore.parameter_set(new_parameters)
+    assert present_parameters is not new_parameters
