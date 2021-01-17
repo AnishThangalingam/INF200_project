45eb7b1fc196efb60bf36ab049f07948b9e0858e
Majorann Kode <majorann.thevarajah@nmbu.no>
2021-01-17 11:37:55 +0100

    Adding a test to check new born weight

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -15,3 +15,15 @@ def test_parameter_set():
     present_parameters = Herbivore.parameters
     new_parameters = Herbivore.parameter_set(new_parameters)
     assert present_parameters is not new_parameters
+
+
+def test_weight_at_birth():
+    """
+    Testing if the animals weight at birth is higher than 0 when
+    we start by inserting None.
+    """
+    herbivore = Herbivore(0, None)
+    assert herbivore.weight > 0
+
+    carnivore = Carnivore(0, None)
+    assert carnivore.weight > 0
