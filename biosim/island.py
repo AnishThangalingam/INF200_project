99acf47bcf1d89aa5c3655690c1febf740f98344
anishthangalingam <anish.thangalingam@nmbu.no>
2021-01-18 01:13:28 +0100

    added some new functions to the island script

--- biosim/island.py
+++ biosim/island.py
@@ -13,7 +13,11 @@ class Island:
     landscapes = {"W": Water, "D": Desert, "H": Highland, "L": Lowland}
 
     def __init__(self, island_map, initial_population=None):
+        """
 
+        :param island_map:
+        :param initial_population:
+        """
         self.amount_of_herbivores = []
         self.amount_of_carnivores = []
         self.map = {}
@@ -25,16 +29,72 @@ class Island:
         self.map_creating()
         self.population_in_cell(self.ini_pop)
 
-        for each_line in self.line_island:
-            for type in each_line:
-                if type not in self.landscapes.keys():
-                    raise ValueError("One or more of the cell type does not exit")
+    def check_boundary_and_invalid_landscape(self):
+        """
+        Legg inn kommentarer
+
+        :return:
+        """
+        for pos in range(len(self.line_island)):
+            if self.line_island[pos][-1] != "W":
+                raise ValueError("Boundary, Island is not surrounded by water")
+            elif self.line_island[pos][0] != "W":
+                raise ValueError("Boundary, Island is not surrounded by water")
+
+        for lines in self.line_island:
+            for cell_type in lines:
+                if cell_type not in self.landscapes.keys():
+                    raise ValueError("Cell type does not exist")
+
+    def check_map_lines(self):
+        """
+        Legg inn kommentar
+        :return:
+        """
+        for line in self.line_island:
+            if len(self.line_island[0]) != len(line)
+                raise ValueError("All the lines in the island map must have the same length")
+
 
     def map_creating(self):
         """
         Creates a map
         """
+
+        self.check_boundary_and_invalid_landscape()
+        self.check_map_lines()
         for y_coord, line in enumerate(self.line_island):
-            for type, x_coord in enumerate(line):
-                self.map[(y_coord + 1, x_coord + 1)] = self.landscapes[type]()
+            for cell_type, x_coord in enumerate(line):
+                self.map[(y_coord + 1, x_coord + 1)] = self.landscapes[cell_type]()
         return self.map
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    herb_
