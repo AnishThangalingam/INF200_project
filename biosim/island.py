6205bc35e7cbc0ed8872ea4fb6c78cfb3d9cf713
anishthangalingam <anish.thangalingam@nmbu.no>
2021-01-19 00:17:49 +0100

    Changed some of the island functions and created doc strings to the functions

--- biosim/island.py
+++ biosim/island.py
@@ -1,4 +1,12 @@
 # -*- encoding: utf-8 -*-
+"""
+This island script contains an island class, which gives us functions that creates
+the map of Rossumøya. The island class also contain happenings that occur on the island
+every year.
+
+Purpose of this function is to give detailed information about the island and the annual cycle
+of the island. Which can be used to form the island map.
+"""
 
 __author__ = "Anish Thangalingam, Majorann Thevarjah"
 __email__ = "anish.thangalingam@nmbu.no ,Majorann.thevarajah@nmbu.no"
@@ -10,13 +18,19 @@ from .landscape import Water, Desert, Highland, Lowland
 
 
 class Island:
+    """
+    Class for the whole island, which tells us what the island contains and this class forms the
+    island
+    """
+
     landscapes = {"W": Water, "D": Desert, "H": Highland, "L": Lowland}
 
     def __init__(self, island_map, initial_population=None):
         """
+        This function gives the opportunity to update the parameters
 
-        :param island_map:
-        :param initial_population:
+        :param island_map: the map of Rossumøya
+        :param initial_population: The population in the island
         """
         self.amount_of_herbivores = []
         self.amount_of_carnivores = []
@@ -31,9 +45,10 @@ class Island:
 
     def check_boundary_and_invalid_landscape(self):
         """
-        Legg inn kommentarer
+        This function checks if the island is surrounded by water. if it is not this function raises a valueerror,
+        which tells us what is wrong. This function also checks if a cell type exists, if it does not exist, the
+        function raises a valueerror.
 
-        :return:
         """
         for pos in range(len(self.line_island)):
             if self.line_island[pos][-1] != "W":
@@ -48,16 +63,53 @@ class Island:
 
     def check_map_lines(self):
         """
-        Legg inn kommentar
-        :return:
+        This function checks if all the map lines have the same length, which they must have. If not this function
+        raises a valueerror, which tells us that all the lines must have the same length.
+
         """
         for line in self.line_island:
             if len(self.line_island[0]) != len(line):
                 raise ValueError("All the lines in the island map must have the same length")
 
+    @staticmethod
+    def move_to_cell(loc_pos):
+        """
+        This function gives the animal in the cell which coordinates it can move to. The information we are given is
+        that it can go either up, down, left or right. So we make a list with those four neighbour cells.
+
+        :param: loc_pos, the locations position of the cell
+        :return: Random cell from the four neighbouring cells
+        """
+        x_coord = loc_pos[1]
+        y_coord = loc_pos[0]
+
+        neighbour_cells_loc = [(y_coord + 1, x_coord), (y_coord - 1, x_coord),
+                               (y_coord, x_coord + 1), (y_coord, x_coord - 1)]
+
+        random_cell_from_list = random.choice(neighbour_cells_loc)
+
+        return random_cell_from_list
+
+    def population_in_cell(self, population):
+        """
+        This function gives us the population of a particular cell and sets a population in the given current
+        location. So the function sets a population for the animals in a particular cell.
+
+        :param: population, the population of animals in a cell
+        """
+        current_population = population
+
+        for animal in current_population:
+            pop = animal["pop"]
+            loc = animal["loc"]
+            self.map[loc].set_a_population[pop]
+
     def map_creating(self):
         """
-        Creates a map
+        This function creates the island map, taking into account that it checks boundary, invalid lanscapes
+        and map lines.
+
+        return: map, it returns the created map
         """
 
         self.check_boundary_and_invalid_landscape()
@@ -67,43 +119,13 @@ class Island:
                 self.map[(y_coord + 1, x_coord + 1)] = self.landscapes[cell_type]()
         return self.map
 
-    @staticmethod
-    def move_to_cell(loc_pos):
-        """
-        gives the coordinates to which cell the animal can move to, it can go either up, down, left or right
-        """
-        x_coord = loc_pos[1]
-        y_coord = loc_pos[0]
-
-        neighbour_cells_loc = [(x_coord + 1, y_coord), (x_coord - 1, y_coord),
-                               (x_coord, y_coord + 1), (x_coord, y_coord - 1)]
-
-        Random_cell_from_list = random.choice(neighbour_cells_loc)
-
-        return Random_cell_from_list
-
-    def population_in_cell(self, population):
-        """
-        Gives us the population in a particular cell
-
-        :return:
-        """
-        if population is None:
-            current_population = self.ini_pop
-        else:
-            current_population = population
-
-        for animal in current_population:
-            pop = animal["pop"]
-            loc = animal["loc"]
-            self.map[loc].set_a_population[pop]
-
     def migration(self, loc_pos):
         """
-        Make a function for the migrating animals, where flag is the passable landscape for the animals. This for loop
-        makes the animal migrate to a suited landscape, if it is not a suitable landscape the for loop will break
+        This function makes a animal migrate, where flag is the passable landscape for the animal. So if the landscape
+        is suitable for the animal it will migrate there, if not it will move on to another random neighbour cell. When
+        the animal migrate it will be added to the new location and removed from the old.
 
-        :return:
+        :param: loc_pos, the locations position of the cell
         """
 
         if self.map[loc_pos].flag:
@@ -112,32 +134,27 @@ class Island:
 
             for herbivore in migrating_herbivore:
                 new_loc = self.move_to_cell(loc_pos)
-                if self.map[new_loc].flag:
+                if not self.map[new_loc].flag:
+                    break
+                else:
                     self.map[new_loc].population_Herbivore.append(herbivore)
                     self.map[loc_pos].population_Herbivore.remove(herbivore)
-                else:
-                    break
 
             for carnivore in migrating_carnivore:
                 new_loc = self.move_to_cell(loc_pos)
-                if self.map[new_loc].flag:
+                if not self.map[new_loc].flag:
+                    break
+                else:
                     self.map[new_loc].population_Carnivore.append(carnivore)
                     self.map[loc_pos].population_Carnivore.remove(carnivore)
-                else:
-                    break
 
     def island_season_cycle(self):
         """
-        the given cycle for a year, this is what happens a season, every functions that happens in a cell during a
-        season
-        :return:
+        This function gives us the cycle for a year. This is functions work annually and works for all the cells in
+        the island.
         """
 
         for loc_pos in self.map:
-            self.map[loc_pos].new_parameter_set()
-            self.map[loc_pos].set_a_population()
-            self.map[loc_pos].get_number_of_herbivores()
-            self.map[loc_pos].get_number_of_carnivores()
             self.map[loc_pos].animal_aging()
             self.map[loc_pos].animal_death()
             self.map[loc_pos].animal_weight_loss()
@@ -146,4 +163,4 @@ class Island:
             self.map[loc_pos].set_food_parameters()
             self.map[loc_pos].herbivore_eat()
             self.map[loc_pos].carnivore_eat()
-            self.map[loc_pos].animal_migrate()
+            self.migration(loc_pos)
--- tests/test_island.py
+++ tests/test_island.py
@@ -4,3 +4,25 @@ __author__ = "Anish Thangalingam, Majorann Thevarjah"
 __email__ = "anish.thangalingam@nmbu.no ,Majorann.thevarajah@nmbu.no"
 
 
+def test_check_boundary_and_invalid_landscape():
+    """
+
+    :return:
+    """
+
+
+
+"""
+def test_check_map_lines():
+
+def test_map_creating():
+
+def test_move_to_cell():
+
+def test_population_in_cell():
+
+def test_migration():
+
+def test_island_season_cycle():
+
+"""
